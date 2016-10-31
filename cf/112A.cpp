af19e87854a9eb539889287d14f8c4ad6618758a
storm95 <krankit007@gmail.com>
2016-10-31 22:14:31 +0530

    Use to upper with right syntex

--- cf/112A.cpp
+++ cf/112A.cpp
@@ -9,6 +9,9 @@ int main(){
     for(int i=0;i<len;i++){
         str1[i]=toupper(str1[i]);
         str2[i]=toupper(str2[i]);
+        /*
+        str1[i]=toupper(str1[i]);
+        */
     }
     if(str1<str2)cout<<-1<<endl;
     else if(str1==str2)cout<<0<<endl;
